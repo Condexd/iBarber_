a25aefa026b7973f21e891f7a25c86c8f9d91ae1
olwpab <luiss_ortiz@soy.sena.edu.co>
2023-09-10 19:08:39 -0500

    Configuracion

--- ibarber/Front-end/vite-project/vite.config.js
+++ ibarber/Front-end/vite-project/vite.config.js
@@ -1,7 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
+  base: '/inicio', // Configura la ruta base a "/inicio"
   plugins: [react()],
-})
+  // Resto de tu configuración de Vite
+});

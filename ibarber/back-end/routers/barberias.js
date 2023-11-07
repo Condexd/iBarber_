33527a4ab44c621944cb2279b869b0d9cb8f73a3
olwpad <olwpad@gmail.com>
2023-11-06 22:32:33 -0500

    Se Añadieron Endpoints y se arregló un error

--- ibarber/back-end/routers/barberias.js
+++ ibarber/back-end/routers/barberias.js
@@ -3,23 +3,27 @@ const router = Router();
 //registro de barberia
 router.post("/barberia", async (req, res) => {
   try {
-    const { nombre_barberia, direccion_barberia, nombre_ciudad } = req.body;
+    const { nombre_barberia, direccion_barberia, ciudad, barberos } = req.body;
 
-    const data = new BarberiaModel({
-      nombre_barberia: nombre_barberia,
-      direccion_barberia: direccion_barberia,
-      ciudad: {
-        nombre_ciudad: nombre_ciudad,
-      },
+    // Verifica que los campos obligatorios estén presentes
+    if (!nombre_barberia || !direccion_barberia || !ciudad || !barberos) {
+      return res.status(400).json({ message: "Todos los campos son obligatorios" });
+    }
+    const newBarberia = new BarberiaModel({
+      nombre_barberia,
+      direccion_barberia,
+      ciudad,
+      barberos,
     });
 
-    const result = await data.save();
-    res.status(200).json({ message: "Registro exítoso" });
+    const result = await newBarberia.save();
+    res.status(201).json({ message: "Registro exitoso", data: result });
   } catch (error) {
-    console.error("Error al registrar barbero:", error);
+    console.error("Error al registrar la barbería:", error);
     res.status(500).json({ message: "Error interno del servidor" });
   }
 });
+
 
 router.get('/barberias', async (req, res) => {
   try {
@@ -35,9 +39,46 @@ router.get('/barberias', async (req, res) => {
   }
 });
 
+router.get('/barberia/:id', async (req, res) => {
+  const barberiaId = req.params.id;
+
+  try {
+    const barberia = await BarberiaModel.findById(barberiaId);
+    if (barberia) {
+      res.status(200).json(barberia);
+    } else {
+      res.status(404).json({ message: 'Barbería no encontrada' });
+    }
+  } catch (error) {
+    console.error('Error al obtener la barbería:', error);
+    res.status(500).json({ message: 'Error interno del servidor' });
+  }
+});
+
+
+router.get('/barberia/:id/barberos', async (req, res) => {
+  const barberiaId = req.params.id;
+
+  try {
+    const barberia = await BarberiaModel.findById(barberiaId);
+    if (barberia) {
+      const barberos = barberia.barberos;
+      res.status(200).json(barberos);
+    } else {
+      res.status(404).json({ message: 'Barbería no encontrada' });
+    }
+  } catch (error) {
+    console.error('Error al obtener la lista de barberos de la barbería:', error);
+    res.status(500).json({ message: 'Error interno del servidor' });
+  }
+});
+
+
+
+
 router.put('/barberia/:id', async (req, res) => {
   const barberiaId = req.params.id;
-  const { nombre_barberia, direccion_barberia, nombre_ciudad } = req.body;
+  const { nombre_barberia, direccion_barberia,ciudad} = req.body;
 
   try {
     const updatedBarberia = await BarberiaModel.findByIdAndUpdate(
@@ -46,7 +87,7 @@ router.put('/barberia/:id', async (req, res) => {
         nombre_barberia: nombre_barberia,
         direccion_barberia: direccion_barberia,
         ciudad: {
-          nombre_ciudad: nombre_ciudad,
+          nombre_ciudad: ciudad.nombre_ciudad,
         },
       },
     );

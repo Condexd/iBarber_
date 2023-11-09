4d6c4908468a8a02783a8d5b4e3d0fa546af0bcc
olwpad <olwpad@gmail.com>
2023-11-09 10:40:36 -0500

    se Agregó inicio de Endpoints

--- ibarber/back-end/Modulos/barril.js
+++ ibarber/back-end/Modulos/barril.js
@@ -9,10 +9,11 @@ import usuarioModel from "../models/usuario.js";
 import BarberiaModel from "../models/barberias.js";
 import {correoelectronico} from "./html.js"
 import { enviarCorreo } from "./nodemailer.js";
+import citaModel from "../models/citas.js"
 
 
 
 
 
 
-export { Router, bcrypt, nodemailer, crypto, jwt, dotenv, usuarioModel, BarberiaModel,correoelectronico,enviarCorreo };
+export { Router, bcrypt, nodemailer, crypto, jwt, dotenv, usuarioModel, BarberiaModel,correoelectronico,enviarCorreo,citaModel };
---
+++ ibarber/back-end/routers/citas.js
@@ -0,0 +1,68 @@
+import { Router, dotenv, citaModel } from "../Modulos/barril.js";
+
+dotenv.config();
+const router = Router();
+
+
+router.post('/api/citas', async (req, res) => {
+    try {
+      const nuevaCita = new citaModel(req.body);
+      const resultado = await nuevaCita.save();
+      res.json(resultado);
+    } catch (error) {
+      res.status(500).json({ error: 'Error al crear la cita' });
+    }
+  });
+  
+
+router.get('/api/citas', async (req, res) => {
+    try {
+      const citas = await citaModel.find();
+      res.json(citas);
+    } catch (error) {
+      res.status(500).json({ error: 'Error al obtener las citas' });
+    }
+  });
+
+router.get('/api/citas/:id', async (req, res) => {
+    try {
+      const cita = await citaModel.findById(req.params.id);
+      if (!cita) {
+        return res.status(404).json({ error: 'Cita no encontrada' });
+      }
+      res.json(cita);
+    } catch (error) {
+      res.status(500).json({ error: 'Error al obtener la cita' });
+    }
+  });
+
+
+router.put('/api/citas/:id', async (req, res) => {
+    try {
+      const citaActualizada = await citaModel.findByIdAndUpdate(
+        req.params.id,
+        req.body,
+        { new: true }
+      );
+      if (!citaActualizada) {
+        return res.status(404).json({ error: 'Cita no encontrada' });
+      }
+      res.json(citaActualizada);
+    } catch (error) {
+      res.status(500).json({ error: 'Error al actualizar la cita' });
+    }
+  });
+  
+
+router.delete('/api/citas/:id', async (req, res) => {
+    try {
+      const resultado = await citaModel.findByIdAndDelete(req.params.id);
+      if (!resultado) {
+        return res.status(404).json({ error: 'Cita no encontrada' });
+      }
+      res.json({ mensaje: 'Cita eliminada correctamente' });
+    } catch (error) {
+      res.status(500).json({ error: 'Error al eliminar la cita' });
+    }
+  });
+  

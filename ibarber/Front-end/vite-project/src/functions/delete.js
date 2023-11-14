08e247c02553ae78cb44047a498d96c3ac2fee72
olwpad <olwpad@gmail.com>
2023-11-13 22:34:48 -0500

    Pequeño cambio de mostrar error

--- ibarber/Front-end/vite-project/src/functions/delete.js
+++ ibarber/Front-end/vite-project/src/functions/delete.js
@@ -1,3 +1,4 @@
+import { mostrarMensajeError } from "../modulos/alertas";
 import { API_URLS } from "../modulos/urls";
 export const deleteBarber=async(barberoId)=>{
     try {
@@ -14,7 +15,7 @@ export const deleteBarber=async(barberoId)=>{
           return true
         } else {
           // Maneja el caso en que la petición no fue exitosa
-          console.error('Error al borrar el barbero');
+          mostrarMensajeError('Error al borrar el barbero');
         }
       } catch (error) {
         // Maneja errores de red u otros errores relacionados con la petición

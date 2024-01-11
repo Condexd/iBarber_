ca24c8b544e93ba30dadf8d8d0afb3dfd1685969
olwpad <olwpad@gmail.com>
2024-01-11 16:20:56 -0500

    Se implementó segundo router para quitar cabecero en algunas rutas

--- ibarber/Front-end/vite-project/src/App.jsx
+++ ibarber/Front-end/vite-project/src/App.jsx
@@ -1,15 +1,18 @@
 import { useState, useEffect } from "react";
-import {useNavigate } from 'react-router-dom';
-import Cabecero from "./componentes/layout/cabecero";
+import { useNavigate } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 import Rutas from "./componentes/rutas";
+import Registrar from "./componentes/auth/Registrar";
+import IniciarSesion from "./componentes/auth/inicioSesion";
+import { RecuperarContraseña } from "./componentes/auth/RecuperarContraseña";
 
 function App() {
-  const navigate=useNavigate();
+  const navigate = useNavigate();
   // Estado para verificar si el usuario está autenticado
   const [isAuthenticated, setIsAuthenticated] = useState(false);
   // Efecto para verificar la autenticación al cargar la página
-  useEffect(() => { 
-    const token = localStorage.getItem('token');
+  useEffect(() => {
+    const token = localStorage.getItem("token");
     // Verificar si hay un token almacenado en localStorage (puedes personalizar esto)
     if (token) {
       setIsAuthenticated(true);
@@ -18,20 +21,34 @@ function App() {
   // Función para cerrar sesión
   const logout = () => {
     // Eliminar el token al cerrar sesión
-    localStorage.removeItem('token');
+    localStorage.removeItem("token");
     localStorage.removeItem("userData");
     setIsAuthenticated(false);
     navigate("/");
   };
 
   return (
-      <><Cabecero isAuthenticated={isAuthenticated} logout={logout} /><Rutas isAuthenticated={isAuthenticated}
-       setIsAuthenticated={setIsAuthenticated}
-       logout={logout}
-        /></>
-
+    <>
+      <Routes>
+        <Route path="/registrarse" element={<Registrar />} />
+        <Route path="/recuperar-contrasena" element={<RecuperarContraseña />} />
+        <Route
+          path="/Login"
+          element={<IniciarSesion setIsAuthenticated={setIsAuthenticated} />}
+        />
+        <Route
+          path="/*"
+          element={
+            <Rutas
+              isAuthenticated={isAuthenticated}
+              setIsAuthenticated={setIsAuthenticated}
+              logout={logout}
+            />
+          }
+        />
+      </Routes>
+    </>
   );
 }
 
 export default App;
-
--- ibarber/Front-end/vite-project/src/componentes/rutas.jsx
+++ ibarber/Front-end/vite-project/src/componentes/rutas.jsx
@@ -1,9 +1,6 @@
 import { Navigate, Route, Routes } from "react-router-dom";
 import Inicio from "./home/inicio";
 import Perfil from "./Perfil/perfil";
-import Registrar from "./auth/Registrar";
-import IniciarSesion from "./auth/inicioSesion";
-import { RecuperarContraseña } from "./auth/RecuperarContraseña";
 import { Crearbarberia } from "./Barberia/crearbarberia";
 import { MiBarberia } from "./Barberia/miBarberia";
 import { MisBarberos } from "./barberos/MisBarberos";
@@ -11,35 +8,29 @@ import { CrearBarbero } from "./barberos/CrearBarbero";
 import { CitaForm } from "./citas/CitaForm";
 import { MisCitas } from "./citas/MisCitas";
 
+import Cabecero from "./layout/cabecero";
 
-
-function Rutas({ isAuthenticated, setIsAuthenticated,logout }) {
+function Rutas({ isAuthenticated, logout }) {
   return (
-    <Routes>
-      <Route
-        path="/Home"
-        element={<Inicio isAuthenticated={isAuthenticated} />}
-      />
-      <Route path="/" element={<Navigate to="/Home" />} />
-      {isAuthenticated ? (
-        <>
-          <Route path="/perfil" element={<Perfil logout={logout} />} />
-          <Route path="/mi-barberia" element={<MiBarberia />} />
-          <Route path="/empleados" element={<MisBarberos />} />
-          <Route path="/new-empleado" element={<CrearBarbero />} />
-          <Route path="/new-barberia" element={<Crearbarberia />} />
-          <Route path="/new-cita" element={<CitaForm />} />
-          <Route path="/citas" element={<MisCitas />} />
-        </>
-      ) : (
-        <Route
-          path="/Login"
-          element={<IniciarSesion setIsAuthenticated={setIsAuthenticated} />}
-        />
-      )}
-      <Route path="/registrarse" element={<Registrar />} />
-      <Route path="/recuperar-contrasena" element={<RecuperarContraseña />} />
-    </Routes>
+    <>
+      <Cabecero isAuthenticated={isAuthenticated} logout={logout} />
+      <Routes>
+        <Route path="/Home" element={<Inicio isAuthenticated={isAuthenticated} />} />
+        <Route path="/" element={<Navigate to="/Home" />} />
+
+        {isAuthenticated && (
+          <>
+            <Route path="/perfil" element={<Perfil logout={logout} />} />
+            <Route path="/mi-barberia" element={<MiBarberia />} />
+            <Route path="/empleados" element={<MisBarberos />} />
+            <Route path="/new-empleado" element={<CrearBarbero />} />
+            <Route path="/new-barberia" element={<Crearbarberia />} />
+            <Route path="/new-cita" element={<CitaForm />} />
+            <Route path="/citas" element={<MisCitas />} />
+          </>
+        )}
+      </Routes>
+    </>
   );
 }
 

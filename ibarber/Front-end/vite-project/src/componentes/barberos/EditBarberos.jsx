1e05c9a91d8020cbc58d650438104ff136b4516e
Condexd <camiloconde622@gmail.com>
2024-02-08 12:55:59 -0500

    se actualizo los campos y diseño de editar en mis barberos

--- ibarber/Front-end/vite-project/src/componentes/barberos/EditBarberos.jsx
+++ ibarber/Front-end/vite-project/src/componentes/barberos/EditBarberos.jsx
@@ -4,10 +4,8 @@ import { API_URLS } from '../../modulos/urls';
 
 export const EditBarberos = ({ barbero, funcionEditar, setVisible, usuario }) => {
   const [formData, setFormData] = useState({
-    biografia_barbero: '',
     especialidad: '',
     experiencia: '',
-    num_barbero: '',
   });
 
   const [isModalOpen, setModalOpen] = useState(false);
@@ -25,18 +23,16 @@ export const EditBarberos = ({ barbero, funcionEditar, setVisible, usuario }) =>
 
   const handleSubmit = async (e) => {
     e.preventDefault();
-    const { experiencia, biografia_barbero, especialidad, num_barbero } = formData;
+    const { experiencia, especialidad} = formData;
     const result = await actualizar(
-      { experiencia, biografia_barbero, especialidad, barbero, num_barbero },
+      { experiencia, especialidad, barbero},
       `${API_URLS.actualizarBarbero}/${usuario}`
     );
     if (funcionEditar(result)) {
       setModalOpen(false);
       setFormData({
-        biografia_barbero: '',
         especialidad: '',
         experiencia: '',
-        num_barbero: '',
       });
     }
     setVisible(false);
@@ -45,10 +41,8 @@ export const EditBarberos = ({ barbero, funcionEditar, setVisible, usuario }) =>
   const closeModal = () => {
     setModalOpen(false);
     setFormData({
-      biografia_barbero: '',
       especialidad: '',
       experiencia: '',
-      num_barbero: '',
     });
     setVisible(false);
   };
@@ -61,18 +55,6 @@ export const EditBarberos = ({ barbero, funcionEditar, setVisible, usuario }) =>
         </span>
         <h2 className="edit">{barbero}</h2>
         <div className="form-content">
-          <div className="contenedor-password">
-            <label htmlFor="biografia_barbero">Biografía barbero:</label>
-            <textarea
-              className="biografia-input" // Añadido para el estilo
-              type="text"
-              id="biografia_barbero"
-              name="biografia_barbero"
-              value={formData.biografia_barbero}
-              onChange={handleChange}
-              rows="10" // Ajusta el número de filas
-            />
-          </div>
           <div className="contenedor-edit">
             <label htmlFor="especialidad">Especialidad:</label>
             <input
@@ -94,17 +76,7 @@ export const EditBarberos = ({ barbero, funcionEditar, setVisible, usuario }) =>
               onChange={handleChange}
             />
           </div>
-          <div className="contenedor-edit">
-            <label htmlFor="num_barbero">Número barbero:</label>
-            <input
-              type="number"
-              id="num_barbero"
-              name="num_barbero"
-              value={formData.num_barbero}
-              onChange={handleChange}
-            />
-          </div>
-          <button className="change-password-btn" onClick={handleSubmit}>
+          <button className="change-info-btn" onClick={handleSubmit}>
             Guardar Cambios
           </button>
         </div>

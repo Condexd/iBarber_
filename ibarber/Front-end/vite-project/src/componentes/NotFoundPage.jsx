0473492863f778eb6bd4609a4045fce64e4f0fab
Davida4200 <ekhizde99@gmail.com>
2024-02-07 19:26:29 -0500

    Added 404 NF images, new route and created notFound.css

---
+++ ibarber/Front-end/vite-project/src/componentes/NotFoundPage.jsx
@@ -0,0 +1,11 @@
+import "../Estilos/notFound.css"
+
+export const NotFoundPage = () => {
+  return (
+    <main className="contenedor-not-found">
+      <div>
+        <img src="https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.shutterstock.com%2Fes%2Fsearch%2Fastronauta-saludando&psig=AOvVaw1iAFxhck27S1VNSBq2YZWs&ust=1707437865324000&source=images&cd=vfe&opi=89978449&ved=0CBIQjRxqFwoTCOjBwpq8moQDFQAAAAAdAAAAABAg" />
+      </div>
+    </main>
+  )
+}

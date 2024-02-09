2db8a4c12350152b5c2398f7af3340f90a0e4461
tiagor17 <santiagocas73@gmail.com>
2024-02-08 22:18:05 -0500

    Se agrego la vista de no citas

--- ibarber/Front-end/vite-project/src/componentes/citas/Nocitas.jsx
+++ ibarber/Front-end/vite-project/src/componentes/citas/Nocitas.jsx
@@ -1,11 +1,21 @@
-import React from 'react'
+import React from 'react';
+import { API_URLS } from "../../modulos/urls";
+import { useNavigate } from "react-router-dom";
 
 export const Nocitas = () => {
+  const navigate = useNavigate();
+
+  function nocitasre() {
+    navigate("/Home");
+  }
+
   return (
-    <section className='d-flex justify-content-center containerCita  align-items-center'>
-        <div className=''>
-        No hay citas para mostrar.
-        </div>
+    <section className='d-flex justify-content-center containerCita align-items-center'>
+      <div className='text-center'>
+        <img className="mx-auto" src={`${API_URLS.obtenerImage}/uploads/nocitas.png`} alt="No hay citas agendadas" style={{ maxWidth: '250px' }} /> {/* La imagen con un ancho máximo de 250px */}
+        <div><strong style={{ fontSize: '1.5rem' }}>¡Aún no tienes una cita agendada!</strong></div> {/* El texto debajo de la imagen con negrilla y un tamaño de fuente de 1.5rem */}
+        <button onClick={nocitasre} className="ver-btn"><strong>Agendar cita</strong></button> {/* Botón para agendar una cita con el color especificado y el texto en negrilla */}
+      </div>
     </section>
-  )
-}
+  );
+};

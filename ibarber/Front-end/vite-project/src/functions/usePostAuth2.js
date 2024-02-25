e0740926ef84c4df65fd669187f6d28dde9e367b
olwpad <olwpad@gmail.com>
2024-02-24 19:40:31 -0500

    handled error

--- ibarber/Front-end/vite-project/src/functions/usePostAuth2.js
+++ ibarber/Front-end/vite-project/src/functions/usePostAuth2.js
@@ -1,7 +1,6 @@
-
 export const enviadorAuth2 = async (url, valor) => {
   try {
-    const token = localStorage.getItem("token"); 
+    const token = localStorage.getItem("token");
     const response = await fetch(url, {
       method: 'POST',
       headers: {
@@ -16,9 +15,11 @@ export const enviadorAuth2 = async (url, valor) => {
       return data;
     } else {
       const errorResponse = await response.json();
+      throw new Error(errorResponse.message); // Lanza un error con el mensaje del servidor
     }
   } catch (error) {
     console.error('Error en la solicitud:', error);
     mostrarMensajeErrorInesperado();
+    throw error; // Propaga el error para que pueda ser manejado en el componente que llama a enviadorAuth2
   }
 };

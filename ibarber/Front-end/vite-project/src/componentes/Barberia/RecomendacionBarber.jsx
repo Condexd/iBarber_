a78257835d954d6a218979c19c485a5882137b9f
olwpad <olwpad@gmail.com>
2024-03-17 14:57:25 -0500

    design was added

---
+++ ibarber/Front-end/vite-project/src/componentes/Barberia/RecomendacionBarber.jsx
@@ -0,0 +1,64 @@
+import React from 'react'
+import ScrollAnimation from 'react-animate-on-scroll';
+import { API_URLS } from "../../modulos/urls";
+export const RecomendacionBarber = () => {
+  return (
+    <ScrollAnimation
+    animateIn="animate__animated animate__fadeIn"
+    duration={0.9}
+    animateOnce
+  >
+    <section className="recommendations-section ">
+      <div>
+      <h2 style={{ color: "black" }}>
+        ¡Prepárate para abrir tu propia barbería!
+      </h2>
+      <div className="recommendation-content animate__animated animate__fadeIn">
+        <img
+          src={`${API_URLS.obtenerImage}/uploads/boceto.jpg`}
+          alt="Recomendaciones para abrir una barbería"
+        />
+        <div className="recommendation-text">
+          <p>
+            ¡Felicidades por dar el paso para convertirte en propietario de
+            una barbería! Aquí tienes algunas recomendaciones para
+            asegurarte de que tu nuevo negocio sea un éxito:
+          </p>
+          <ul>
+            <li>
+              <strong>Investiga tu mercado:</strong> Analiza la demanda de
+              servicios de barbería en tu área y conoce a tu competencia.
+            </li>
+            <li>
+              <strong>Define tu propuesta única:</strong> Diferénciate
+              ofreciendo servicios especiales o un ambiente único.
+            </li>
+            <li>
+              <strong>Crea una experiencia memorable:</strong> Haz que cada
+              visita a tu barbería sea especial con un servicio excepcional
+              y un ambiente acogedor.
+            </li>
+            <li>
+              <strong>Marketing inteligente:</strong> Utiliza las redes
+              sociales y otras herramientas de marketing para promocionar tu
+              negocio y llegar a más personas.
+            </li>
+            <li>
+              <strong>Planifica tu gestión financiera:</strong> Lleva un
+              control de tus gastos e ingresos para mantener la salud
+              financiera de tu negocio.
+            </li>
+          </ul>
+          <p>
+            Recuerda que abrir una barbería es emocionante, pero también
+            requiere esfuerzo y dedicación. ¡Estamos seguros de que tendrás
+            mucho éxito en esta nueva aventura!
+          </p>
+        </div>
+      </div>
+      </div>
+  
+    </section>
+  </ScrollAnimation>
+  )
+}
--- ibarber/Front-end/vite-project/src/componentes/Barberia/crearbarberia.jsx
+++ ibarber/Front-end/vite-project/src/componentes/Barberia/crearbarberia.jsx
@@ -1,126 +1,151 @@
-import { useForm } from '../../Hooks/useform';
-import { API_URLS } from '../../modulos/urls';
-import { UserContext } from '../context/UserContext';
-import { useNavigate } from 'react-router-dom';
-import { useContext ,useState} from 'react';
-import { enviadorAuth } from '../../functions/usePostAuth';
-
+import { useForm } from "../../Hooks/useform";
+import { API_URLS } from "../../modulos/urls";
+import { UserContext } from "../context/UserContext";
+import { useNavigate } from "react-router-dom";
+import { useContext, useState } from "react";
+import { enviadorAuth } from "../../functions/usePostAuth";
+import { RecomendacionBarber } from "./RecomendacionBarber";
 export const Crearbarberia = () => {
   const { userData, setUserData } = useContext(UserContext);
   const [usuario, setUsuario] = useState(userData.usuario);
 
   const { formState, funcion } = useForm({
-    nombre_barberia: '',
-    direccion_barberia: '',
-    nombre_ciudad: '',
-    descripcion_barberia: '',
-    email: '',
-    telefono: ''
+    nombre_barberia: "",
+    direccion_barberia: "",
+    nombre_ciudad: "",
+    descripcion_barberia: "",
+    email: "",
+    telefono: "",
   });
-  const { nombre_barberia, direccion_barberia, nombre_ciudad, descripcion_barberia, email, telefono } = formState;
+  const {
+    nombre_barberia,
+    direccion_barberia,
+    nombre_ciudad,
+    descripcion_barberia,
+    email,
+    telefono,
+  } = formState;
 
   const navigate = useNavigate();
   const handleSubmit = async (e) => {
     e.preventDefault();
-    if(nombre_ciudad==="seleccionar" || nombre_ciudad==="")return;
-    const result = await enviadorAuth(API_URLS.BARBERIA, { nombre_barberia, direccion_barberia, nombre_ciudad, usuario, descripcion_barberia, email, telefono});
-    if(result){
-      navigate("/mi-barberia")
+    if (nombre_ciudad === "seleccionar" || nombre_ciudad === "") return;
+    const result = await enviadorAuth(API_URLS.BARBERIA, {
+      nombre_barberia,
+      direccion_barberia,
+      nombre_ciudad,
+      usuario,
+      descripcion_barberia,
+      email,
+      telefono,
+    });
+    if (result) {
+      navigate("/mi-barberia");
     }
-    setUserData(prevUserData => ({
+    setUserData((prevUserData) => ({
       ...prevUserData, // Clonar las propiedades existentes
-      barberia: true // Cambiar la propiedad "barberia" a false
+      barberia: true, // Cambiar la propiedad "barberia" a false
     }));
   };
 
   return (
     <>
-
       <main id="form-barber-container">
-        <div className='img-container'>
-        <img src="http://localhost:3300/uploads/image.png" className='animate__animated animate__fadeInLeft'  loading="lazy" alt="Imagen de la barbería" /> 
+        <div className="subContainer">
+          <picture className="containerImage">
+            <img
+              src={`${API_URLS.obtenerImage}/uploads/fondbar.png`}
+              alt=""
+              srcset=""
+              className="animate__animated animate__fadeInLeft"
+            />
+          </picture>
+          <section className="containerForm   animate__animated animate__fadeInRight">
+            <form onSubmit={handleSubmit} id="form-barber">
+              <div id="login-title">
+                <h3 className="title-barber">Crear Barbería</h3>
+              </div>
+              <div className="login-username">
+                <input
+                  type="text"
+                  name="nombre_barberia"
+                  placeholder="Nombre de la barbería"
+                  required
+                  clasNam="nombre_barberia"
+                  value={nombre_barberia}
+                  onChange={funcion}
+                />
+              </div>
+              <div className="login-username">
+                <input
+                  type="text"
+                  name="direccion_barberia"
+                  placeholder="Dirección de la barbería"
+                  required
+                  id="direccion_barberia"
+                  value={direccion_barberia}
+                  onChange={funcion}
+                />
+              </div>
+              <div className="login-username">
+                <input
+                  type="text"
+                  name="descripcion_barberia"
+                  placeholder="Descripción de la barbería"
+                  required
+                  id="descripcion_barberia"
+                  value={descripcion_barberia}
+                  onChange={funcion}
+                />
+              </div>
+              <div className="login-username">
+                <input
+                  type="email"
+                  name="email"
+                  placeholder="Email de la barbería"
+                  required
+                  id="email"
+                  value={email}
+                  onChange={funcion}
+                />
+              </div>
+              <div className="login-username">
+                <input
+                  type="text"
+                  name="telefono"
+                  placeholder="Teléfono de la barbería"
+                  required
+                  id="telefono"
+                  value={telefono}
+                  onChange={funcion}
+                />
+              </div>
+              <div className="login-city">
+                <div className="city-title-container">
+                  <label className="titleCity">Nombre de la ciudad</label>
+                </div>
+                <select
+                  name="nombre_ciudad"
+                  value={nombre_ciudad}
+                  onChange={funcion}
+                  id="nombre_ciudad"
+                >
+                  <option value="seleccionar">Seleccionar</option>
+                  <option value="Bogotá">Bogotá</option>
+                  <option value="Medellín">Medellín</option>
+                  <option value="Cali">Cali</option>
+                </select>
+              </div>
+              <div id="btn-enviar-barbero">
+                <button type="submit" id="boton-crear-barberia">
+                  Enviar
+                </button>
+              </div>
+            </form>
+          </section>
         </div>
-        <form onSubmit={handleSubmit} id="form-barber">
-          <div id="login-title">
-            <h3 className='title-barber'>Crear Barbería</h3>
-          </div>
-          <div id="login-username">
-            <input
-              type="text"
-              name="nombre_barberia"
-              placeholder="Nombre de la barbería"
-              required
-              id="nombre_barberia"
-              value={nombre_barberia}
-              onChange={funcion}
-            />
-          </div>
-          <div id="login-password">
-            <input
-              type="text"
-              name="direccion_barberia"
-              placeholder="Dirección de la barbería"
-              required
-              id="direccion_barberia"
-              value={direccion_barberia}
-              onChange={funcion}
-            />
-          </div>
-          <div id="login-username">
-            <input
-              type="text"
-              name="descripcion_barberia"
-              placeholder="Descripción de la barbería"
-              required
-              id="descripcion_barberia"
-              value={descripcion_barberia}
-              onChange={funcion}
-            />
-          </div>
-          <div id="login-username">
-            <input
-              type="email"
-              name="email"
-              placeholder="Email de la barbería"
-              required
-              id="email"
-              value={email}
-              onChange={funcion}
-            />
-          </div>
-          <div id="login-username">
-            <input
-              type="text"
-              name="telefono"
-              placeholder="Teléfono de la barbería"
-              required
-              id="telefono"
-              value={telefono}
-              onChange={funcion}
-            />
-          </div>
-          <div id="login-username">
-            <label>Nombre de la ciudad</label>
-            <select
-              name="nombre_ciudad"
-              value={nombre_ciudad}
-              onChange={funcion}
-              id="nombre_ciudad"
-            >  <option value="seleccionar">Seleccionar</option>
-              <option value="Bogotá">Bogotá</option>
-              <option value="Medellín">Medellín</option>
-              <option value="Cali">Cali</option>
-            </select>
-          </div>
-          <div id="btn-enviar-barbero">
-            <button type="submit" id="boton-crear-barberia">
-              Enviar
-            </button>
-          </div>
-        </form>
       </main>
+      <RecomendacionBarber/>
     </>
   );
 };
-
-
